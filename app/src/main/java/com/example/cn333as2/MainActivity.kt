cc8b5a7926da2e50cdf5883eaebe85ee34c57293
Poottipong Rueangsuntikornkul <6210520117@studen.tu.ac.th>
2022-02-21 21:38:18 +0700

    AddButton

--- app/src/main/java/com/example/cn333as2/MainActivity.kt
+++ app/src/main/java/com/example/cn333as2/MainActivity.kt
@@ -2,17 +2,42 @@ package com.example.cn333as2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.cn333as2.databinding.MainActivityBinding
 import com.example.cn333as2.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding:MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.add_button, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.addButton.setOnClickListener {
+            showCreateListName()
+        }
     }
+
+    private fun showCreateListName() {
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(getString(R.string.name_of))
+        builder.setView(listTitleEditText)
+        builder.setPositiveButton(getString(R.string.create_list)) {
+            dialog, _->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
 }
