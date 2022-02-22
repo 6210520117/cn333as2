2b2b045d82100a344161b5d631f5f58ae000464e
Poottipong Rueangsuntikornkul <6210520117@studen.tu.ac.th>
2022-02-22 23:03:42 +0700

    Try add Detail in list

--- app/src/main/java/com/example/cn333as2/ListDetailActivity.kt
+++ app/src/main/java/com/example/cn333as2/ListDetailActivity.kt
@@ -2,13 +2,18 @@ package com.example.cn333as2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cn333as2.models.NameList
 import com.example.cn333as2.ui.detail.ListDetailFragment
 
 class ListDetailActivity : AppCompatActivity() {
+    lateinit var  list : NameList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_detail_activity)
+        list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)!!
+        title = list.name
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, ListDetailFragment.newInstance())
--- app/src/main/java/com/example/cn333as2/ui/detail/ListDetailFragment.kt
+++ app/src/main/java/com/example/cn333as2/ui/detail/ListDetailFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cn333as2.R
+import com.example.cn333as2.databinding.ListDetailFragmentBinding
 
 class ListDetailFragment : Fragment() {
+    lateinit var binding: ListDetailFragmentBinding
 
     companion object {
         fun newInstance() = ListDetailFragment()
@@ -20,7 +22,8 @@ class ListDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.list_detail_fragment, container, false)
+        binding = ListDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
